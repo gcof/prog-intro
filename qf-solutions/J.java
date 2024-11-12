24e2a909d694f29594b23e29aab4485c42a59e0e
gcof <a9282044449@gmail.com>
2024-11-12 17:16:56 +0300

    task_10

--- qf-solutions/J.java
+++ qf-solutions/J.java
@@ -3,35 +3,38 @@ import java.util.Scanner;
 public class J {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n;
-        n = scanner.nextInt();
-        scanner.nextLine();
-        int[][] matrix = new int[n][n];
-        int[][] graph = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            String toReadInput = scanner.nextLine();
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = toReadInput.charAt(j) - '0';
+        try{
+            int n;
+            n = scanner.nextInt();
+            scanner.nextLine();
+            int[][] matrix = new int[n][n];
+            int[][] graph = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                String toReadInput = scanner.nextLine();
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = toReadInput.charAt(j) - '0';
+                }
             }
-        }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (matrix[i][j] == 1) {
-                    graph[i][j] = 1;
-                    for (int k = j + 1; k < n; k++) {
-                        matrix[i][k] = (matrix[i][k] - matrix[j][k] + 10) % 10;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (matrix[i][j] == 1) {
+                        graph[i][j] = 1;
+                        for (int k = j + 1; k < n; k++) {
+                            matrix[i][k] = (matrix[i][k] - matrix[j][k] + 10) % 10;
+                        }
                     }
                 }
             }
-        }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(graph[i][j]);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(graph[i][j]);
+                }
+                System.out.println();
             }
-            System.out.println();
+        } finally {
+            scanner.close();
         }
-        scanner.close();
     }
 }
